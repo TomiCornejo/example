a569732755e25fc6b392d774a1b51c8730726e21
unknown <tcornejo.z888@gmail.com>
2023-02-05 12:22:54 -0300

    input text sencillo

---
+++ main.js
@@ -0,0 +1,8 @@
+function showInfo(){
+    let data = document.getElementById("inputText").value;
+    alert(data);
+}
+
+// window.addEventListener("load", ()=>{
+//     alert("hello world!");
+// });
